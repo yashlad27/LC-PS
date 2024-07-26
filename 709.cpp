bb9d56e225241fd504c83c85e5becfaaafbed915
Yash Lad <71868797+yashlad27@users.noreply.github.com>
2024-07-26 17:06:39 +0530

    709

--- 709.cpp
+++ 709.cpp
@@ -1,3 +1,14 @@
-#include<stdio.h>
-using namespace std;
+// Check each character: Use a loop to go through each character of the string.
+// Convert if uppercase: If a character is between 'A' and 'Z', convert it to lowercase by adding 32 to its ASCII value.
 
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0; i<s.length(); i++){
+            if(s[i]>='A' && s[i]<='Z'){
+                s[i]=s[i]+32;
+            }
+        }
+        return s;
+    }
+};
